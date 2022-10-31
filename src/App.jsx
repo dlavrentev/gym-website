886ad8b1950f6ce254442c34332298c5f40f1cf7
Dimitri Lavrentev <101797662+dlavrentev@users.noreply.github.com>
2022-10-31 15:47:17 +0100

    created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-
+import About from './pages/about/About.jsx';
+import Contact from './pages/contact/Contact.jsx';
+import Gallery from './pages/gallery/Gallery.jsx';
+import Home from './pages/home/Home.jsx';
+import NotFound from './pages/notFound/NotFound.jsx';
+import Plans from './pages/plans/Plans.jsx';
+import Trainers from './pages/trainers/Trainers.jsx';
 
 const App = () => {
   return (
-    <div>HElllooo</div>
+    <div>
+      <Home />
+      <About />
+      <Contact />
+      <Gallery />
+      <Plans />
+      <Trainers />
+      <NotFound />
+    </div>
   )
 }
 
