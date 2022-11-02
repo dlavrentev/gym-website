c0776bee67021661a808e89739b1b8a523ecbfa1
Dimitri Lavrentev <101797662+dlavrentev@users.noreply.github.com>
2022-11-02 18:19:58 +0100

    set onclick function hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../images/logo.png";
 import { links } from "../data";
 import { GoThreeBars } from "react-icons/go";
 import "./navbar.css";
 
+
+
 const Navbar = () => {
+const [isNavShowing, setIsNavShowing] = useState(false);
+
   return (
     <nav>
       <div className="container nav__container">
         <Link to="/home" className="logo">
           <img src={Logo} alt="Nav Logo" />
         </Link>
-        <ul className="nav__links">
+        <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}`}>
           {links.map(({ name, path }, index) => {
             return (
               <li>
@@ -20,7 +25,7 @@ const Navbar = () => {
             );
           })}
         </ul>
-        <button className="nav__toggle-btn">
+        <button className="nav__toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
           <GoThreeBars />
         </button>
       </div>
